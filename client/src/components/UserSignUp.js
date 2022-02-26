f9221089cf29755333ed67f61055ba5d68a5531f
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-02-25 22:14:42 -0700

    updated to allow users to sign up

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,9 +1,12 @@
 // UserSignUp - This component provides the "Sign Up" screen by rendering a form that allows a user to sign up by creating a new account. The component also renders a "Sign Up" button that when clicked sends a POST request to the REST API's /api/users route and signs in the user. This component also renders a "Cancel" button that returns the user to the default route (i.e. the list of courses).
 
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const UserSignUp = () => {
+const UserSignUp = (props) => {
+  // state
+  const [errors, setErrors] = useState([]);
+
   // refs
   const firstNameRef = useRef();
   const lastNameRef = useRef();
@@ -19,19 +22,55 @@ const UserSignUp = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const body = {
+    const { context } = props;
+    const user = {
       firstName: firstNameRef.current.value,
       lastName: lastNameRef.current.value,
       emailAddress: emailAddressRef.current.value,
       password: passwordRef.current.value,
     };
-    console.log(body);
+    context.data
+      .createUser(user)
+      .then((errors) => {
+        if (errors.length) {
+          console.log("ERRORS THEN: ", errors);
+          setErrors(errors);
+        } else {
+          setErrors([]);
+          firstNameRef.current.value = "";
+          lastNameRef.current.value = "";
+          emailAddressRef.current.value = "";
+          passwordRef.current.value = "";
+          console.log(`${user.emailAddress} has successfully signed up!`);
+          context.actions.signIn(user.emailAddress, user.password);
+        }
+      })
+      .catch((err) => {
+        console.error("USERSIGNUP.js: ", err);
+        this.props.history.push("/error");
+      });
+  };
+
+  // display any form validation errors
+  const displayErrors = () => {
+    if (errors.length) {
+      const errorsList = errors.map((error) => <li>{error}</li>);
+
+      return (
+        <div className="validation--errors">
+          <h3>Validation Errors</h3>
+          <ul>{errorsList}</ul>
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
     <main>
       <div className="form--centered">
         <h2>Sign Up</h2>
+        {displayErrors()}
         <form onSubmit={handleSubmit}>
           <label htmlFor="firstName">First Name</label>
           <input
