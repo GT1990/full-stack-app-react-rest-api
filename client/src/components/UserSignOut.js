27ec1988e3850a1b4cf9d4fc7d84fbeadb2b3c9a
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-02-26 20:42:48 -0700

    added comments

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,9 +1,21 @@
-// UserSignOut - This component is a bit of an oddball as it doesn't render any visual elements. Instead, it signs out the authenticated user and redirects the user to the default route (i.e. the list of courses).
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import React, { Component } from "react";
+/**
+ * UserSignOut calls the signout function passed in by context prop.
+ * The signout function in Context.js sets the authenticatedUser state to null,
+ * add deletes the authenticatedUser cookie, finally redirecting to default route.
+ * @param {object} props - destructuring context
+ * @returns null - does NOT render anything, redirects to default route
+ */
+const SignOut = ({ context }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    context.actions.signOut();
+    navigate("/");
+  });
 
-export default class SignOut extends Component {
-  render() {
-    return <h1>Sign Out</h1>;
-  }
-}
+  return null;
+};
+
+export default SignOut;
