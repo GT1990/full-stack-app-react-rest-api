b58c8c4750050d929a39cfaf7c7c5f425a298ca9
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-02-27 03:02:16 -0700

    added component and css for loading animation

---
+++ client/src/components/Loading.js
@@ -0,0 +1,9 @@
+import "../loading.css";
+const Loading = () => {
+  return (
+    <div className="loadingWrapper">
+      <div className="loader"></div>
+    </div>
+  );
+};
+export default Loading;
